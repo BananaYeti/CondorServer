962b1bb7766c0d25055cad150e8e0fee9a3a6e07
Michael Kelly <rockymkelly@gmail.com>
2018-05-09 18:00:23 -0400

    Made chat work with actual rooms

--- app/socketchat/socketchat.js
+++ app/socketchat/socketchat.js
@@ -1,10 +1,30 @@
+const DEFAULT_CHAT = "global";
+
 function openSockets(io){
-    io.on('connection', (client) =>{
-        console.log('client connected');
-        client.on('message', (data)=>{
-            io.emit('message', data);
-            console.log(data);
-        });
+  io.of('/chat').on('connection', (socket) =>{
+      console.log('client connected');
+      socket.join(DEFAULT_CHAT);
+      socket.room =DEFAULT_CHAT;
+
+      socket.on('join', function(room, ack){
+        var oldRoom = socket.room;
+        if(oldRoom){
+          socket.leave(oldRoom);
+        }
+        socket.room = room;
+        socket.join(room);
+        ack();
+      });
+
+
+      socket.on('message', function(message){
+        var room = socket.room
+        if (room){
+          io.of('/chat').to(room).emit('broadcast', message);
+        } else {
+          socket.emit('error', 'not in a chat room');
+        }
+      });
     });
 }
 
